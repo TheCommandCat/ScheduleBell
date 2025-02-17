eaabddc5d08c02f8674c002bff433f723cb5d990
Nir Chen <bhrtkhvji@gmail.com>
2025-02-17 19:47:31 +0200

    reformated and cleaned admin.tsx

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -6,84 +6,20 @@ import {
   TextField,
   Button,
   Box,
-  DialogTitle,
 } from "@mui/material";
-import SchedulerEditor from "../components/SchedulerEditor";
 import SendIcon from "@mui/icons-material/Send";
+import SchedulerEditor from "../components/SchedulerEditor";
 
 const AdminPage: React.FC = () => {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [logedin, setLogedin] = useState(false);
   const [password, setPassword] = useState("");
-  const [scheduleFile, setScheduleFile] = useState<File | null>(null);
   const [soundFile, setSoundFile] = useState<File | null>(null);
   const [message, setMessage] = useState("");
   const [schedule, setSchedule] = useState<{ [key: string]: string }>({});
-  const [initialSchedule, setInitialSchedule] = useState<{
-    [key: string]: string;
-  }>({});
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
-  };
-
-  const handleScheduleSubmit = async (schedule: { [key: string]: string }) => {
-    console.log("called handleScheduleSubmit frontend");
-
-    try {
-      const response = await fetch("/api/updateSchedule", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer 1234",
-        },
-        body: JSON.stringify(schedule),
-      });
-
-      if (response.ok) {
-        const result = await response.json();
-        console.log("Schedule updated successfully");
-      } else {
-        const error = await response.json();
-        console.error("Failed to update schedule:", error.error);
-      }
-    } catch (error) {
-      console.error("Error submitting schedule:", error);
-    }
-  };
-
-  const fetchInitialSchedule = async () => {
-    try {
-      const response = await fetch("/api/getSchedule");
-      if (response.ok) {
-        const data = await response.json();
-        setInitialSchedule(data);
-        setSchedule(data); // Initialize current schedule with initial data
-      } else {
-        console.error("Failed to fetch initial schedule");
-      }
-    } catch (error) {
-      console.error("Error fetching initial schedule:", error);
-    }
-  };
-
-  useEffect(() => {
-    if (logedin) {
-      fetchInitialSchedule();
-    }
-  }, [logedin]);
-
-  const handleRemoveSoundFile = () => {
-    setSoundFile(null);
-    if (fileInputRef.current) {
-      fileInputRef.current.value = "";
-    }
-  };
-
-  const handleSoundFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setSoundFile(e.target.files[0]);
-    }
   };
 
   const handleLogin = () => {
@@ -95,18 +31,45 @@ const AdminPage: React.FC = () => {
     }
   };
 
-  const handleSubmit = async () => {
-    if (!logedin) {
-      setMessage("You must be logged in to upload files");
-      return;
-    }
+  // fetch the initial schedule
 
-    if (Object.keys(schedule).length > 0) {
-      handleScheduleSubmit(schedule);
+  const handleSoundFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setSoundFile(e.target.files[0]);
     }
+  };
 
+  const handleRemoveSoundFile = () => {
+    setSoundFile(null);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+  };
+
+  const handleScheduleSubmit = async (schedule: { [key: string]: string }) => {
+    try {
+      const response = await fetch("/api/updateSchedule", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${password}`,
+        },
+        body: JSON.stringify(schedule),
+      });
+
+      setMessage(
+        response.ok
+          ? "Schedule updated successfully"
+          : "Failed to update schedule"
+      );
+    } catch (error) {
+      console.error("Error submitting schedule: ", error);
+    }
+  };
+
+  const handleAudioUpload = async () => {
     if (!soundFile) {
-      setMessage("Please select a sound file to upload or update the schedule");
+      setMessage("Please select a sound file to upload");
       return;
     }
 
@@ -116,7 +79,7 @@ const AdminPage: React.FC = () => {
         headers: {
           "Content-Disposition": `form-data; name="soundFile"; filename="${soundFile.name}"`,
           "Content-Type": soundFile.type,
-          Authorization: "Bearer 1234",
+          Authorization: `Bearer ${password}`,
         },
         body: soundFile,
       });
@@ -136,6 +99,19 @@ const AdminPage: React.FC = () => {
     }
   };
 
+  const handleSubmit = async () => {
+    if (!logedin) {
+      setMessage("You must be logged in to upload files");
+      return;
+    }
+
+    if (Object.keys(schedule).length > 0) {
+      await handleScheduleSubmit(schedule);
+    }
+
+    await handleAudioUpload();
+  };
+
   return (
     <>
       {logedin ? (
@@ -153,7 +129,6 @@ const AdminPage: React.FC = () => {
               מערכת מנהל
             </Typography>
             <Box sx={{ display: "flex", flexDirection: "column" }}>
-              {/* Column layout for Schedule Editor and Upload */}
               <Box
                 sx={{
                   display: "flex",
@@ -163,7 +138,6 @@ const AdminPage: React.FC = () => {
                   marginBottom: 2,
                 }}
               >
-                <DialogTitle>Edit Schedule</DialogTitle>
                 <SchedulerEditor
                   onScheduleChange={(newSchedule) => setSchedule(newSchedule)}
                 />
