de9ccdc3d507764b3fd964856a3bb358065db625
Nir Chen <bhrtkhvji@gmail.com>
2025-03-01 23:16:32 +0200

    removed unnecessary semicolon in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <CssBaseline />
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </ThemeProvider>
     </>
   );
